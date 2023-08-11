d888c8061d548f909dfc3cf27f7708fca2dd64ed
ahmaddzidnii <zdngaming312@gmail.com>
2023-08-11 19:04:55 +0700

    p

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
       <BrowserRouter>
             <NavbarComponent/>
         <Routes>
-          <Route path ='/checkidgame' element ={<CheckId/>}/>
+          <Route path ='/' element ={<CheckId/>}/>
           
         </Routes>
       </BrowserRouter>
