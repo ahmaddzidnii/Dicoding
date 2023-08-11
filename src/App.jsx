6781c6b460b76420df0d31c95fe38e3c4caddaf6
ahmaddzidnii <zdngaming312@gmail.com>
2023-08-11 19:01:25 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import {BrowserRouter, Routes ,Route} from 'react-router-dom'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+
+import data from './data.json'
+import CheckId from './Pages/CheckId'
+import NavbarComponent from './Components/NavbarComponents'
+
+
+const App = () => {
+
+  return (
+    <div>
+      <BrowserRouter>
+            <NavbarComponent/>
+        <Routes>
+          <Route path ='/checkidgame' element ={<CheckId/>}/>
+          
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
