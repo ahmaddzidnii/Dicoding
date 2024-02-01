b1cffaa577af254e7f2bb4edaa98381371786fbe
ahmaddzidnii <zdngaming312@gmail.com>
2024-02-01 21:35:13 +0700

    submission:  mengerjakan submission belajar web dasar dikoding

--- Frontend/Submission Tugas Akhir Membuat Website/assets/scripts/script.js
+++ Frontend/Submission Tugas Akhir Membuat Website/assets/scripts/script.js
@@ -1,16 +1,9 @@
 const yearElement = document.getElementById("year");
 
-/** Fungsi Untuk Mengambil Tahun Sekarang, fungsi ini mengembalikan number */
 const getYearNow = () => {
   return new Date().getFullYear();
 };
 
 yearElement.textContent = getYearNow();
 
-const getAnime = async () => {
-  const response = await fetch("data/jjk.json");
-  const data = await response.json();
-  console.log(data);
-};
-
-getAnime();
+const date = new Date();
