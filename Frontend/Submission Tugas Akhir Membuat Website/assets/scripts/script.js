b7e375ae8052aa9a4ed513e8b59af84d83cdcdbf
ahmaddzidnii <zdngaming312@gmail.com>
2024-02-01 22:55:47 +0700

    submission: menyelesaikan submission

--- Frontend/Submission Tugas Akhir Membuat Website/assets/scripts/script.js
+++ Frontend/Submission Tugas Akhir Membuat Website/assets/scripts/script.js
@@ -6,4 +6,24 @@ const getYearNow = () => {
 
 yearElement.textContent = getYearNow();
 
-const date = new Date();
+const clockElement = document.getElementById("clock");
+setInterval(() => {
+  const date = new Date();
+  const minute = date.getMinutes();
+  const hour = date.getHours();
+  const seconds = date.getSeconds();
+
+  const formatDate = (h, m, s) => {
+    if (h < 10) {
+      h = `0${h}`;
+    }
+    if (m < 10) {
+      m = `0${m}`;
+    }
+    if (s < 10) {
+      s = `0${s}`;
+    }
+    return `${h}:${m}:${s}`;
+  };
+  clockElement.textContent = formatDate(hour, minute, seconds);
+}, 1000);
